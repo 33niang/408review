46872901f592398c296a5478080db6ac25f62d5f
三 三 <duwei20020501@qq.com>
2025-05-26 07:08:36 +0800

    原地逆置

--- 19-2.c
+++ 19-2.c
@@ -1,16 +1,59 @@
 /*
-顺序表L长度为n
-要求时间复杂度o(n),空间复杂度o(1)
-删除表中所有值为x的元素 
+要求将顺序表L所有元素逆置
+空间复杂度为o(1)
 */
 
 #include<stdio.h>
 #define MAXSIZE 50
 
 typedef struct SqList {
-	int a[MAXSIZE];
-	int length;
+    int a[MAXSIZE];
+    int length;
 }SqList;
 
 
+int reverse(SqList* L) {
+    if (L == NULL) {
+        printf("空指针\n");
+        return 0;
+    }
+    int i = 0;
+    int temp;
+    for (i = 0; i < L->length / 2; i++) {
+        temp = L->a[L->length - i - 1];
+        L->a[L->length - i - 1] = L->a[i];
+        L->a[i] = temp;
+    }
+    return 1;
+}
+
+void print_sqlist(SqList L) {
+    int i = 0;
+    for (int i = 0; i < L.length; i++) {
+        printf("%d ", L.a[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    SqList L;
+    L.a[0] = 2;
+    L.a[1] = 3;
+    L.a[2] = 5;
+    L.a[3] = 7;
+    L.a[4] = 1;
+    L.a[5] = 9;
+    L.a[6] = 8;
+    L.a[7] = 55;
+    L.length = 8;
+
+    print_sqlist(L);
+    if (reverse(&L) == 0) {
+        printf("失败\n");
+    }
+    else {
+        print_sqlist(L);
+    }
+    return 0;
+}
 
