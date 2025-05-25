ed27af4a4b98f4003257e020ea1ae5ed838a44a8
三 三 <duwei20020501@qq.com>
2025-05-26 06:55:20 +0800

    删除顺序表中值为x的元素

--- 19-2.c
+++ 19-2.c
@@ -1,6 +1,7 @@
 /*
-要求将顺序表L所有元素逆置
-时间复杂度为o(1)
+顺序表L长度为n
+要求时间复杂度o(n),空间复杂度o(1)
+删除表中所有值为x的元素 
 */
 
 #include<stdio.h>
@@ -10,4 +11,6 @@ typedef struct SqList {
 	int a[MAXSIZE];
 	int length;
 }SqList;
+
+
 
