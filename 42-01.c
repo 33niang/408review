f47659cf2269c7d62ba4fed9a62a3de98b71f130
三三 <2663307689@qq.com>
2025-05-30 01:13:23 +0800

    linlist delete all x

--- 42-01.c
+++ 42-01.c
@@ -3,6 +3,7 @@
 */
 
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef struct lnode {
 	int data;
@@ -11,49 +12,77 @@ typedef struct lnode {
 
 int a[4] = { 1,2,3,4 };
 int n = 4;
-void buildlist(lnode* L) {
-	lnode* s, * r = L;
-	r->data = a[0];
-	if (n == 1)
-		r->next = NULL;
-	else {
-		for (int i = 0; i < n; i++) {
-			s = (lnode*)malloc(sizeof(lnode));
-			s->data = a[i];
+
+//递归实现
+void buildlist(linklist* L) {//创建无头结点的链表
+	*L = NULL;
+	lnode* r = NULL;//尾指针
+	for (int i = 0; i < n; i++) {
+		lnode* s = (lnode*)malloc(sizeof(lnode));
+		s->data = a[i];
+		s->next = NULL;
+		if (*L == NULL) {//如果此时头结点刚初始化，那么就把s赋给头结点
+			*L = s;
+			r = s;
+		}
+		else {
+			//如果此时链表中已经有了元素
+			// 那就将尾指针指向新申请的s节点
+			//并且将尾指针的位置移向s
 			r->next = s;
-			r = r->next;
+			r = s;
 		}
 	}
 }
 
-void disp(lnode* L) {
+void disp(linklist L) {
 	lnode* s = L;
+	if (!s) {
+		printf("empty\n");
+		return;
+	}
 	while (s) {
 		printf("%d ", s->data);
 		s = s->next;
 	}
+	printf("\n");
 }
 
-void deletex(lnode* L, int x) {
-	if (L == NULL)
-		return;
-	lnode* p;
-	if (L->data == x) {
-		p = L;
-		L = L->next;
+void deletex(linklist* L, int x) {
+	if (*L == NULL)
+		return;//空链表
+	lnode* p = *L;
+	if (p->data == x) {
+		*L = p->next;
 		free(p);
 		deletex(L, x);
 	}
-	else
-		deletex(L->next, x);
+	else {
+		deletex(&((*L)->next), x);//当前节点非x，递归处理下个节点
+	}
 }
+
+void freelist(linklist* L) {
+	lnode* p = *L;
+	while (p) {
+		lnode* temp = p;
+		p = p->next;
+		free(temp);
+	}
+	*L = NULL;
+}
+
+
 int main()
 {
-	lnode list;
-	lnode* L = &list;
-	buildlist(L);
+	linklist L = NULL;
+	buildlist(&L);
+	printf("original\n");
 	disp(L);
-	deletex(L, 2);
+	deletex(&L, 2);
+	printf("after\n");
 	disp(L);
+	freelist(&L);
+
 	return 0;
 }
