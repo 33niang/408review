e9a41b5e60a268cae567e8f6445a090b04b2048e
三三 <2663307689@qq.com>
2025-05-30 02:35:07 +0800

    带头结点递归删除能跑，但是会额外输出一个空表，看不出来哪有问题，懒得改了

--- 42-01.c
+++ 42-01.c
@@ -14,6 +14,7 @@ int a[4] = { 1,2,3,4 };
 int n = 4;
 
 //递归实现
+//无头结点
 void buildlist(linklist* L) {//创建无头结点的链表
 	*L = NULL;
 	lnode* r = NULL;//尾指针
@@ -72,9 +73,72 @@ void freelist(linklist* L) {
 	*L = NULL;
 }
 
+//有头结点
+void buildheadlist(linklist* L) {
+	*L = (lnode*)malloc(sizeof(lnode));
+	if (*L == NULL) {
+		printf("分配失败\n");
+		return;
+	}
+	(*L)->next = NULL;//头结点
+	lnode* r = *L;//尾指针,指向头结点,不能为NULL
+	for (int i = 0; i < n; i++) {
+		lnode* s = (lnode*)malloc(sizeof(lnode));
+		if (s == NULL) {//判断s分配内存是否成功
+			printf("分配失败\n");
+			return;
+		}
+		s->data = a[i];
+		s->next = NULL;
+		r->next = s;
+		r = s;
+	}
+}
 
+void dispheadlist(linklist L) {
+	if (L == NULL || L->next == NULL) {
+		printf("空表\n");
+		return;
+	}
+	lnode* s = L->next;
+	while (s) {
+		printf("%d ", s->data);
+		s = s->next;
+	}
+	printf("\n");
+}
+
+void deletex_headlist(linklist* L, int x) {
+	if (*L == NULL || (*L)->next == NULL) {
+		printf("空表\n");
+		return;
+	}
+	lnode* p = (*L)->next;
+	lnode* prev = *L;
+	if (p->data == x) {
+		prev->next = p->next;
+		free(p);
+		deletex_headlist(L, x);
+	}
+	else {
+		deletex_headlist(&(prev->next), x);
+	}
+}
+
+void free_headlist(linklist* L) {
+	if (*L == NULL)
+		return;
+	lnode* p = *L;
+	while (p) {
+		lnode* temp = p;
+		p = p->next;
+		free(temp);
+	}
+	*L = NULL;
+}
 int main()
 {
+	
 	linklist L = NULL;
 	buildlist(&L);
 	printf("original\n");
@@ -83,6 +147,15 @@ int main()
 	printf("after\n");
 	disp(L);
 	freelist(&L);
+	
+	printf("--------------\n");
+	linklist L2 = NULL;
+	buildheadlist(&L2);
+	dispheadlist(L2);
+	deletex_headlist(&L2,2);
+	dispheadlist(L2);
+	free_headlist(&L2);
+	
 
 	return 0;
 }
